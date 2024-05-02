ec57492b4082d42fdd6ccc7c495882752ceb4b8c
Rahb1t <rodrigo.rjsmz@gmail.com>
2024-05-02 19:48:13 -0400

    Income/Expense forms changed to buttons that open modals

--- client/src/components/AdTransaction/AddTransaction.js
+++ client/src/components/AdTransaction/AddTransaction.js
@@ -1,12 +1,16 @@
 import React, { useState, useContext } from "react";
 import { GlobalContext } from "../../context/GlobalState";
+import Modal from "react-modal";
 import "./AddTransaction.css";
+
+Modal.setAppElement(document.getElementById("root"));
 
 const AddTransaction = () => {
   const [incomeText, setIncomeText] = useState("");
   const [incomeAmount, setIncomeAmount] = useState("");
   const [expenseText, setExpenseText] = useState("");
   const [expenseAmount, setExpenseAmount] = useState("");
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   const { addTransaction } = useContext(GlobalContext);
 
@@ -25,6 +29,9 @@ const AddTransaction = () => {
     // Reset input fields
     setIncomeText("");
     setIncomeAmount("");
+
+    // Close the modal
+    setModalIsOpen(false);
   };
 
   const handleExpenseSubmit = (e) => {
@@ -42,6 +49,22 @@ const AddTransaction = () => {
     // Reset input fields
     setExpenseText("");
     setExpenseAmount("");
+
+    // Close the modal
+    setModalIsOpen(false);
+  };
+
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      width: "50%",
+      height: "50%",
+    },
   };
 
   return (
@@ -49,61 +72,87 @@ const AddTransaction = () => {
       <h3>Add new transaction</h3>
       <div className="transaction-container">
         {/* Income Form */}
-        <div className="transaction-form">
-          <form onSubmit={handleIncomeSubmit}>
-            <div className="form-control">
-              <label htmlFor="income-text">Income Text</label>
-              <input
-                type="text"
-                value={incomeText}
-                onChange={(e) => setIncomeText(e.target.value)}
-                id="income-text"
-                placeholder="Enter income text..."
-              />
+        <div>
+          <button className="btn-income" onClick={() => setModalIsOpen(true)}>
+            Add Income
+          </button>
+
+          <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={() => setModalIsOpen(false)}
+            style={customStyles}
+            contentLabel="Add Income"
+          >
+            <div className="transaction-form">
+              <form onSubmit={handleIncomeSubmit}>
+                <div className="form-control">
+                  <label htmlFor="income-text">Income Text</label>
+                  <input
+                    type="text"
+                    value={incomeText}
+                    onChange={(e) => setIncomeText(e.target.value)}
+                    id="income-text"
+                    placeholder="Enter income text..."
+                  />
+                </div>
+                <div className="form-control">
+                  <label htmlFor="income-amount">Income Amount</label>
+                  <input
+                    type="number"
+                    value={incomeAmount}
+                    onChange={(e) => setIncomeAmount(e.target.value)}
+                    id="income-amount"
+                    placeholder="Enter income amount..."
+                  />
+                </div>
+                <button className="btn" type="submit">
+                  Submit
+                </button>
+              </form>
             </div>
-            <div className="form-control">
-              <label htmlFor="income-amount">Income Amount</label>
-              <input
-                type="number"
-                value={incomeAmount}
-                onChange={(e) => setIncomeAmount(e.target.value)}
-                id="income-amount"
-                placeholder="Enter income amount..."
-              />
-            </div>
-            <button className="btn" type="submit">
-              Add Income
-            </button>
-          </form>
+          </Modal>
         </div>
 
         {/* Expense Form */}
-        <div className="transaction-form">
-          <form onSubmit={handleExpenseSubmit}>
-            <div className="form-control">
-              <label htmlFor="expense-text">Expense Text</label>
-              <input
-                type="text"
-                value={expenseText}
-                onChange={(e) => setExpenseText(e.target.value)}
-                id="expense-text"
-                placeholder="Enter expense text..."
-              />
+        <div>
+          <button className="btn-expense" onClick={() => setModalIsOpen(true)}>
+            Add Expense
+          </button>
+
+          <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={() => setModalIsOpen(false)}
+            style={customStyles}
+            contentLabel="Add Expense"
+          >
+            <div className="transaction-form">
+              <form onSubmit={handleExpenseSubmit}>
+                <div className="form-control">
+                  <label htmlFor="income-text">Expense Text</label>
+                  <input
+                    type="text"
+                    value={expenseText}
+                    onChange={(e) => setExpenseText(e.target.value)}
+                    id="expense-text"
+                    placeholder="Enter expense text..."
+                  />
+                </div>
+                <div className="form-control">
+                  <label htmlFor="expense-amount">Expense Amount</label>
+                  <input
+                    type="number"
+                    value={expenseAmount}
+                    onChange={(e) => setExpenseAmount(e.target.value)}
+                    id="expense-amount"
+                    placeholder="Enter expense amount..."
+                  />
+                </div>
+                <button className="btn" type="submit">
+                  Submit
+                </button>
+              </form>
             </div>
-            <div className="form-control">
-              <label htmlFor="expense-amount">Expense Amount</label>
-              <input
-                type="number"
-                value={expenseAmount}
-                onChange={(e) => setExpenseAmount(e.target.value)}
-                id="expense-amount"
-                placeholder="Enter expense amount..."
-              />
-            </div>
-            <button className="btn" type="submit">
-              Add Expense
-            </button>
-          </form>
+          </Modal>
         </div>
       </div>
     </>
